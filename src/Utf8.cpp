73839b0d464da9be194e04dc7a76d926ee10d848
YaMing Wu <wuyaming@gmail.com>
2019-12-04 02:04:48 -0600

    Add support for valid non-ASCII Unicode code points

--- src/Utf8.cpp
+++ src/Utf8.cpp
@@ -4,7 +4,30 @@
  * This module contains the implementation of the Utf8::Utf8 class.
  */
 
+#include <stddef.h>
 #include <Utf8/Utf8.hpp>
+
+namespace {
+    /**
+     * This computes the logarithm (base 2) of the given integer.
+     *
+     * @param[in] integer
+     *      This is the integer for which to compute the logarithm.
+     *
+     * @return
+     *      The logarithm (base 2) of the given integer is returned.
+     */
+    template< typename I > size_t log2n(I integer) {
+        size_t answer = 0;
+        while (integer > 0)
+        {
+            ++answer;
+            interger >>= 1;
+        }
+
+        return answer;
+    }
+}
 
 namespace Utf8 {
     std::vector<UnicodeCodePoint> AsciiToUnicode(const std::string& ascii) {
@@ -25,10 +48,42 @@ namespace Utf8 {
 
     }
 
+    /*
+        Char. number range | UTF-8 octet sequence
+        (hexadecimal) | (binary)
+        --------------------+---------------------------------------------
+        0000 0000-0000 007F | 0xxxxxxx
+        0000 0080-0000 07FF | 110xxxxx 10xxxxxx
+        0000 0800-0000 FFFF | 1110xxxx 10xxxxxx 10xxxxxx
+        0001 0000-0010 FFFF | 11110xxx 10xxxxxx 10xxxxxx 10xxxxxx
+    */
     std::vector<uint8_t> Utf8::Encode(const std::vector<UnicodeCodePoint>& codePoints) {
         std::vector<uint8_t> encoding;
         for (auto codePoint : codePoints) {
-            encoding.push_back((UnicodeCodePoint)(codePoint & 0x7F));
+            const auto numBits = log2n(codePoint);
+
+            if (numBits <= 7) {
+                encoding.push_back((UnicodeCodePoint)(codePoint & 0x7F));
+            }
+            else if (numBits <= 11) {
+                encoding.push_back((UnicodeCodePoint)(((codePoint >> 6) & 0x1F)) + 0xC0);
+                encoding.push_back((UnicodeCodePoint)(((codePoint) & 0x3F)) + 0x80);
+            }
+            else if (numBits <= 16) {
+                encoding.push_back((UnicodeCodePoint)(((codePoint >> 12) & 0x0F)) + 0xE0);
+                encoding.push_back((UnicodeCodePoint)(((codePoint >> 6) & 0x3F)) + 0x80);
+                encoding.push_back((UnicodeCodePoint)(((codePoint) & 0x3F)) + 0x80);
+            }
+            else if (numBits <= 21) {
+                encoding.push_back((UnicodeCodePoint)(((codePoint >> 18) & 0x07)) + 0xF0);
+                encoding.push_back((UnicodeCodePoint)(((codePoint >> 12) & 0x3F)) + 0x80);
+                encoding.push_back((UnicodeCodePoint)(((codePoint >> 6) & 0x3F)) + 0x80);
+                encoding.push_back((UnicodeCodePoint)(((codePoint) & 0x3F)) + 0x80);
+            }
+            else {
+                // to do
+            }
+
         }
         return encoding;
     }
--- test/src/Utf8Tests.cpp
+++ test/src/Utf8Tests.cpp
@@ -24,3 +24,24 @@ TEST(Utf8Tests, EncodeAscii) {
 
     ASSERT_EQ(expectedCodePoint, actualEncoding);
 }
+
+TEST(Utf8Tests, Symbols) {
+    Utf8::Utf8 utf8;
+    const std::vector< uint8_t > expectedEncoding{ 0x41, 0xE2, 0x89, 0xA2, 0xCE, 0x91, 0x2E };
+    const auto actualEncoding = utf8.Encode({ 0x0041, 0x2262, 0x0391, 0x002E }); // A≢Α.
+    ASSERT_EQ(expectedEncoding, actualEncoding);
+}
+
+TEST(Utf8Tests, EncodeJapanese) {
+    Utf8::Utf8 utf8;
+    const std::vector< uint8_t > expectedEncoding{ 0xE6, 0x97, 0xA5, 0xE6, 0x9C, 0xAC, 0xE8, 0xAA, 0x9E };
+    const auto actualEncoding = utf8.Encode({ 0x65E5, 0x672C, 0x8A9E }); // 日本語
+    ASSERT_EQ(expectedEncoding, actualEncoding);
+}
+
+TEST(Utf8Test, StumpOfTree) {
+    Utf8::Utf8 utf8;
+    const std::vector< uint8_t > expectedEncoding{ 0xF0, 0xA3, 0x8E, 0xB4 };
+    const auto actualEncoding = utf8.Encode({ 0x233B4 }); // 𣎴
+    ASSERT_EQ(expectedEncoding, actualEncoding);
+}
